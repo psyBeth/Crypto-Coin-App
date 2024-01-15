d45bbc6a31fa9b3158b05dad976e2807afd44fd0
Betul <betulkoru06@gmail.com>
2024-01-15 16:15:10 +0300

    apiRequest function file

--- coin-app/main.js
+++ coin-app/main.js
@@ -1,4 +1,5 @@
 import "./scss/style.scss"  //? stylesheet of the project
+import { apiRequest } from "./src/apiRequest";
 
 //!  https://api.coinranking.com/v2/coins
 
@@ -13,19 +14,12 @@ form.addEventListener("submit", (e)=> {
     e.target.reset()    //? delete the form
 })
 
-const getCoinData = async () => {
+const getCoinData = () => {
     const input = document.querySelector("header form input").value
-    const API_KEY = "coinranking734f14b6d044ed7b30d43d513756abfe54db3b39dc88ddc2"
-    const options = { headers: { 'x-access-token': 'API_KEY' } };
-
-    const url = `https://api.coinranking.com/v2/coins?search=${input}`
-    
-    try{
-        const res = await fetch(url, options)
-        const data = await res.json()
-        console.log(json.data.coins[0]);
-    } catch (error){
-        console.log(error);
+    if(!input.trim()){
+        alert("Input cannot be blank.")
+    } else {
+        apiRequest()
     }
 }
 
---
+++ coin-app/src/apiRequest.js
@@ -0,0 +1,14 @@
+export const apiRequest = async () => {
+    const API_KEY = "coinranking734f14b6d044ed7b30d43d513756abfe54db3b39dc88ddc2"
+    const options = { headers: { 'x-access-token': 'API_KEY' } };
+
+    const url = `https://api.coinranking.com/v2/coins?search=${input}`
+
+    try{
+        const res = await fetch(url, options)
+        const data = await res.json()
+        console.log(json.data.coins[0]);
+    } catch (error){
+        console.log(error);
+    }
+}
