006d37de5e55eb965546119e994379fe76c96b2c
Betul <betulkoru06@gmail.com>
2024-01-15 15:57:06 +0300

    getCoinData aync api

--- coin-app/main.js
+++ coin-app/main.js
@@ -10,5 +10,15 @@ form.addEventListener("submit", (e)=> {
     console.log(e.target);
     e.preventDefault()  //? prevent all the submits
     e.target.reset()    //? deleting the form
+    getCoinData()
 })
+
+const getCoinData = async () => {
+    const API_KEY = "coinranking734f14b6d044ed7b30d43d513756abfe54db3b39dc88ddc2"
+    const options = { headers: { 'x-access-token': 'API_KEY' } };
+
+    url = "https://api.coinranking.com/v2/coins?search=bitcoin"
+
+    const res = await fetch()
+}
 
