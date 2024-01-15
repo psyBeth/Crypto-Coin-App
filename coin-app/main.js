84df2cab77e49856f16a64de356d648459dabf7c
Betul <betulkoru06@gmail.com>
2024-01-15 15:51:23 +0300

    submit event form

--- coin-app/main.js
+++ coin-app/main.js
@@ -2,4 +2,13 @@ import "./scss/style.scss"  //? stylesheet of the project
 
 //!  https://api.coinranking.com/v2/coins
 
+//? Selector
+const form = document.querySelector("header form")
+
+//? submit event for the form
+form.addEventListener("submit", (e)=> {
+    console.log(e.target);
+    e.preventDefault()  //? prevent all the submits
+    e.target.reset()    //? deleting the form
+})
 
