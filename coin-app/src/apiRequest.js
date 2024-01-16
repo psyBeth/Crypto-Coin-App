fe21c3ae435b0c079b122867526b45bdb9e14f3d
Betul <betulkoru06@gmail.com>
2024-01-16 23:38:38 +0300

    fixed the json data issue

--- coin-app/src/apiRequest.js
+++ coin-app/src/apiRequest.js
@@ -3,13 +3,13 @@ import { renderCoins } from "./renderData";
 export const apiRequest = async () => {
     const input = document.querySelector("header form input").value
     const API_KEY = "coinranking7ccb2f8ad895d7e07946af5421aa51052c90962d689459af"
-    const options = { headers: { 'x-access-token': 'API_KEY' } };
+    const options = { headers: { 'x-access-token': API_KEY } };
 
     const url = `https://api.coinranking.com/v2/coins?search=${input}`
 
     try{
         const res = await fetch(url, options)
-        const data = await res.json()
+        const json = await res.json()
         if (json.data.coins.length === 0){
             alert("Coin cannot be found.")
         } else {
