5374f15e72b5ce4c7226452a320c26589c9f7fa0
Betul <betulkoru06@gmail.com>
2024-01-16 22:08:39 +0300

    tiny edits

--- coin-app/main.js
+++ coin-app/main.js
@@ -1,14 +1,11 @@
 import "./scss/style.scss"  //? stylesheet of the project
 import { apiRequest } from "./src/apiRequest";
 
-//!  https://api.coinranking.com/v2/coins
-
 //? Selector
 const form = document.querySelector("header form")
 
 //? submit event for the form
 form.addEventListener("submit", (e)=> {
-    console.log(e.target);
     e.preventDefault()  //? prevent all the submits
     getCoinData() //? API request
     e.target.reset()    //? delete the form
